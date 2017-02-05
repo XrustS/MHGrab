f8930c7eb1022eb979e771ef2fb2b917e1cecdc0
Alex Koshelev <xrusts@gmail.com>
2017-02-05 16:49:05 +0500

    Create test for method getRequest

--- test/mHGrab-recive-webdata.js
+++ test/mHGrab-recive-webdata.js
@@ -8,13 +8,44 @@ describe('MHGrab' , function () {
 
             expect(result).to.be.false;
         });
+        it('should return false if first argument is not Object', function () {
+            let grab = new MHGrab();
+            let result ='';
+
+            result = grab.getRequest('str');
+
+            expect(result).to.be.false;
+        });
+        it('should return false if second argument is unset or not Array', function () {
+            let grab = new MHGrab();
+            let result ='';
+
+            result = grab.getRequest({},'str');
+
+            expect(result).to.be.false;
+        });
     });
 });
 
 function MHGrab(){
     let salf = this;
 
-    salf.getRequest = function(){
-        return false;
+    salf.getRequest = function(options, loginParam){
+        if(arguments.length != 0 || !isObject(options) || !Array.isArray(loginParam))
+            return false;
+        return true;
     }
+    function isObject(obj) {
+        if( typeof(obj) === 'object' ){
+            let result = obj.toString()
+                    .replace(']','')
+                    .split(' ')
+                    .pop();
+
+            if ( result === 'Object' )
+            return true;
+            }
+            return false;
+    }
+    return salf;
 }
